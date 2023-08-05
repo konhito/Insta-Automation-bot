96311a540524ffd98e0eca2ef9192f8f212a5b50
konhito <konhito02@gmail.com>
2023-08-05 13:29:40 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+# x = ["aman_danate1", "zenrik_._", "zeus", "hero"]
+# for i in x:
+#     continue
+# print(i)
+
+a = [1, 2, 3, 4, 5]
+
+# printing the list using loop
+for x in range(len(a)):
+    print(a[x])
